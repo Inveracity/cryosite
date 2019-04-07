6b67fe64461eb3e7a9ff02ead4773a561081fe23
Christopher Baklid <christopherb@unity3d.com>
2019-04-07 21:26:19 +0200

    putting social media icons in navbar

--- static/js/cryo.js
+++ static/js/cryo.js
@@ -1,7 +1,17 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip({
         animated: 'fade',
-        placement: 'top',
+        placement: 'bottom',
         html: true
     });
 })
+
+$('#home').click(function () {
+    var val = $(this).val();
+    $('#home-content').slideToggle();
+});
+
+$('#shows').click(function () {
+    var val = $(this).val();
+    $('#shows-content').slideToggle();
+});
